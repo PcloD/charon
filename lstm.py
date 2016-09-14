dc45a448c24a1a4019859a1951a53dcc6a273b96
Shao Kun Deng <deng_shaokun@hotmail.com>
2016-09-14 10:31:18 -0400

    Minor fixes so the program would run

--- lstm.py
+++ lstm.py
@@ -11,12 +11,12 @@ class Model(object):
 		self.label_data = tf.placeholder(tf.float32, [arg.batch_size, 1])
 
 		if arg.lstm:
-			self.cell = tf.nn.rnn_cell.BasicLSTMCell(arg.num_units)
+			self.cell = tf.nn.rnn_cell.BasicLSTMCell(arg.num_units, state_is_tuple=True)
 		else:
 			self.cell = tf.nn.rnn_cell.GRUCell(arg.num_units)
 
 		if arg.num_layers > 1:
-			self.cell = tf.nn.rnn_cell.MultiRNNCell([self.cell] * arg.num_layers)
+			self.cell = tf.nn.rnn_cell.MultiRNNCell([self.cell] * arg.num_layers, state_is_tuple=True)
 		self.cell_state = self.cell.zero_state(arg.batch_size, tf.float32)
 		self.step_count = tf.Variable(0, trainable=False)
 
@@ -24,16 +24,15 @@ class Model(object):
 		outputs, self.cell_state = self.cell(self.input_data, self.cell_state)
 
 		# Map the result to a single scalar
-		self.mappingW = tf.Variable(arg.num_units, 1)
-		self.pressure = tf.tanh(tf.matmul(self.mappingW, outputs))
+		self.mappingW = tf.Variable(tf.random_uniform([arg.num_units, 1], minval=-1, maxval=1, dtype=tf.float32))
+		self.pressure = tf.tanh(tf.matmul(outputs, self.mappingW))
 
 		if trainable:
-			
 			self.loss = tf.squared_difference(self.pressure, self.label_data)
 			trainable_vars = tf.trainable_variables()
 			clipped_grads, _ = tf.clip_by_global_norm(tf.gradients(self.loss, trainable_vars), arg.gradient_clip)
 
-			self.trainer = tf.train.AdamOptimizer(arg.learning_rate).apply_gradient(zip(clipped_grads, trainable_vars), self.step_count)
+			self.trainer = tf.train.AdagradOptimizer(arg.learning_rate).apply_gradients(zip(clipped_grads, trainable_vars), self.step_count)
 
 		self.saver = tf.train.Saver()
 		
@@ -46,6 +45,9 @@ class Model(object):
 			output_var = [self.pressure]
 		
 		output = session.run(output_var, feed_dict=input_feed)
+
+		print session.run(self.label_data, feed_dict=input_feed)
+
 		return output[0]
 
 	def save(self, session, file):
--- train.py
+++ train.py
@@ -2,42 +2,42 @@ import tensorflow as tf
 import numpy as np
 import argparse
 
-import parser
+import data_parser
 import lstm
 
 def main():
 	parser = argparse.ArgumentParser(description="")
-	parser.add_argument('--training_data', type=str, default='Data/Data/bitfinex_hourly.txt', help='Historical price data file to be parsed')
+	parser.add_argument('--training_data', type=str, default='Data/Data/mtgox_hourly.txt', help='Historical price data file to be parsed')
 	parser.add_argument('--save_dir', type=str, default='save', help='Model save directory')
-	parser.add_argument('--lstm', type=bool, default=True)
+	parser.add_argument('--lstm', type=bool, default=False)
 	parser.add_argument('--batch_size', type=int, default=50)
 	parser.add_argument('--num_units', type=int, default=256)
 	parser.add_argument('--num_layers', type=int, default=2)
-	parser.add_argument('--input_length', type=int, default=3)
+	parser.add_argument('--input_length', type=int, default=1)
 	parser.add_argument('--learning_rate', type=float, default=0.1)
 	parser.add_argument('--gradient_clip', type=float, default=5.0)
 
 	parser.add_argument('--price_epsilon', type=float, default=1.0)
 
-	arg = parser.parse()
+	arg = parser.parse_args()
 	train(arg)
 
 def binning(l, k):
 	for i in xrange(0, len(l), k):
-		yield l[i:i+k]
+		yield np.array([l[i:i+k]]).T
 
 def get_input_data(arg):
-	_, price, _, _ = parser.parse(arg.training_data)
-	d = binning(price, arg.batch_size)
+	_, price, _, _ = data_parser.parse(arg.training_data)
+	d = list(binning(price, arg.batch_size))
 	if len(d[-1]) < arg.batch_size:
 		return d[:-1]
 	else:
 		return d
 
 def get_output_data(arg):
-	_, price, _, _ = parser.parse(arg.training_data)
-	label = [1 if price[k+1] > (price[k] + arg.price_epsilon) else (-1 if (price[k+1] < price[k] - arg.price_epsilon) else 0) for k in xrange(len(price) - 1)]
-	d = binning(label, arg.batch_size)
+	_, price, _, _ = data_parser.parse(arg.training_data)
+	label = [1.0 if price[k+1] > (price[k] + arg.price_epsilon) else (-1.0 if (price[k+1] < price[k] - arg.price_epsilon) else 0.0) for k in xrange(len(price) - 1)]
+	d = list(binning(label, arg.batch_size))
 	if len(d[-1]) < arg.batch_size:
 		return d[:-1]
 	else:
@@ -49,6 +49,10 @@ def train(arg):
 	batch_output = get_output_data(arg)
 
 	with tf.Session() as sess:
+		sess.run(tf.initialize_all_variables())
 		for i in xrange(len(batch_input)):
 			loss = model.step(sess, batch_input[i], batch_output[i], trainable=True)
-			print "Iteration {} with loss {}".format(i, loss)
+			print "Iteration {} with loss {}".format(i, np.sum(loss))
+
+if __name__ == '__main__':
+	main()
